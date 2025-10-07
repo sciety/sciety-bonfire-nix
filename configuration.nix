3957fc434153041f39f38b3582067b9b139f5589
Giacomo Leidi <goodoldpaul@autistici.org>
2025-10-07 10:42:42 +0200

    Update to 1.0.0-rc.3.

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
 
   bonfire = {
     flavor = "open_science";
-    version = "1.0.0-rc.2.33";
+    version = "1.0.0-rc.3";
     hostname = "dev-discussions.sciety.org";
     mail-from = "bonfire-admin@sciety.org";
     mail-backend = "sendgrid";
